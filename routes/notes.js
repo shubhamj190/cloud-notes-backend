719f6b31d17f743f6a5616bd44a6bec1f3f97372
shubham <shubham.jadhav@ufaber.com>
2021-11-07 00:13:54 +0530

    update endpoint created and working

--- routes/notes.js
+++ routes/notes.js
@@ -9,7 +9,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   const notes = await Notes.find({ user: req.user });
   res.json(notes);
 });
-
+// <-------------------------------------------------------------------------------------------------------->
 // Route 2: add a new notes using post login required
 router.post(
   "/addnote",
@@ -36,6 +36,58 @@ router.post(
       });
       const savednote = await note.save();
       res.json(savednote);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Internal server error 500", error: error });
+    }
+  }
+);
+
+// <-------------------------------------------------------------------------------------------------------->
+// Route 3: edit notes notes using put request login required
+router.put(
+  "/updatenote/:id",
+  [
+    body("title", "Enter a valid title").isLength({ min: 5 }),
+    body("description", "Enter a valid description").isLength({ min: 5 }),
+  ],
+  fetchuser,
+  async (req, res) => {
+    // if there are any error it will throw the error witht the status
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, description, tag } = req.body;
+      // create a new note object
+      const newNote = {};
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
+
+      // find the note to be update
+
+      let note = await Notes.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("not found");
+      }
+      if (note.user.toString() !== req.user) {
+        console.log("this is true")
+        return res.status(401).send("access denied");
+      }
+      note=await Notes.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true})
+      console.log("this isa note",note)
+      res.json({note})
     } catch (error) {
       res
         .status(500)
